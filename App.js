fa8726ebc601f4f613a00c5bc341c71d8a0a417f
Emmanuel Orozco <emmanuel.orozco@partoo.fr>
2022-07-04 16:17:12 +0200

    Added menu tab navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Feed from './screens/Feed';
+import Screens from './screens/ScreenContainer.js'
 
 export default function App() {
   return (
-    <Feed />
+    <Screens />
   );
 }
 
