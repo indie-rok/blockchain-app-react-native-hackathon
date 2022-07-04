6277b5de1fcc3c6208b64b98cfb9a95e85b296cc
Emmanuel Orozco <emmanuel.orozco@partoo.fr>
2022-07-04 23:10:13 +0200

    added v1 api contract connection

--- screens/MyCards/index.js
+++ screens/MyCards/index.js
@@ -1,25 +1,43 @@
 import React, { useEffect } from 'react'
 import { Text } from 'react-native'
 import { db } from '../../components/FirebaseProvider'
-// import { useCollection } from 'react-firebase-hooks/firestore';
+// import { useCollection } from 'react-firebase-hooks/firestore';=
 
 import { query, collection, getDocs } from "firebase/firestore";
+import { ethers, Wallet } from 'ethers';
+import { Button } from '@rneui/base';
+import testAbi from '../../contracts/abi/test.json'
 
 
 export default function MyCards() {
 
     // const [value] = useCollection(db, 'Rooms')
 
+
+    async function test() {
+        // Connect to the network
+        let provider = ethers.getDefaultProvider('kovan');
+
+        // Load the wallet to deploy the contract with
+
+        let wallet = new ethers.Wallet.fromMnemonic("injury network mandate nature sea apology inside exclude pilot lottery pig sand");
+
+
+        const contract = new ethers.Contract("0x03C15104bb46038Fd7818c4A618F529929F3EFaC", testAbi, provider)
+        console.log(contract)
+        // console.log(await contract.balanceOf(wallet.address))
+    }
+
     useEffect(() => {
-        async function getStuff() {
-            const q = query(collection(db, "Rooms"))
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
-            });
-        }
-        getStuff()
+        // async function getStuff() {
+        //     const q = query(collection(db, "Rooms"))
+        //     const querySnapshot = await getDocs(q);
+        //     querySnapshot.forEach((doc) => {
+        //         // doc.data() is never undefined for query doc snapshots
+        //         console.log(doc.id, " => ", doc.data());
+        //     });
+        // }
+        // getStuff()
     }, [])
 
     // console.log(value)
@@ -32,6 +50,8 @@ export default function MyCards() {
     // );
 
     return (
-        <Text>MyCards</Text>
+        <Text>
+            <Button title="Test" onPress={test} />
+        </Text>
     )
 }
