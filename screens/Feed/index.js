5833d186699cd25ecb5b74858d159ab75429fa20
Emmanuel Orozco <emmanuel.orozco@partoo.fr>
2022-07-04 17:43:09 +0200

    Added working version with photo capture

--- screens/Feed/index.js
+++ screens/Feed/index.js
@@ -1,12 +1,15 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useId } from 'react'
 import { StyleSheet, ScrollView, Text, View } from 'react-native';
 import { Card, Button, Icon } from "@rneui/themed";
 import { Video, AVPlaybackStatus } from 'expo-av';
 
 const videos = [
-    "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/04a1bc0eb98c429ba37b28ddc3f55189.MOV?alt=media&token=bc4466bd-07ed-463a-9cbc-d236114fcf53",
-    "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/0b004fc0097a415d87e69277934cb3fd.MOV?alt=media&token=8b00f737-a89a-41a1-b97d-e57bea8518b7",
-    "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/3ae91185af0a4b30b47c9715c5701256.MOV?alt=media&token=dd57d163-83ec-496d-bb28-e39967ca349d"
+    "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    // "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/04a1bc0eb98c429ba37b28ddc3f55189.MOV?alt=media&token=bc4466bd-07ed-463a-9cbc-d236114fcf53",
+    // "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/0b004fc0097a415d87e69277934cb3fd.MOV?alt=media&token=8b00f737-a89a-41a1-b97d-e57bea8518b7",
+    // "https://firebasestorage.googleapis.com/v0/b/singing-app-f3e35.appspot.com/o/3ae91185af0a4b30b47c9715c5701256.MOV?alt=media&token=dd57d163-83ec-496d-bb28-e39967ca349d"
 ]
 
 export default function Feed() {
@@ -18,8 +21,8 @@ export default function Feed() {
     return (
 
         <ScrollView contentContainerStyle={styles.container}>
-            {videos.map((uri) => {
-                return <View>
+            {videos.map((uri, index) => {
+                return <View key={useId}>
                     <Video
                         ref={video}
                         style={styles.video}
